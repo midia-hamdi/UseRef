344420d302581ec6cfdced7a1fb4c9bda21ae154
midia-hamdi <midiahamdi367@gmail.com>
2023-11-21 17:39:24 +0330

    useref in react

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,18 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
 export default function Counter() {
     const [count, setCount] = useState(0)
+    const countRef = useRef(count)
+
+    useEffect(() => {
+        countRef.current = count       
+    })
   return (
     <div>
         <p style={{color: '#fff'}}>Click Cunt is {count}</p>
-        <button onClick={() => setCount(count + 1)}>Click</button>
+        <button onClick={() => setCount(count + 1)}>Click1</button>
+        <button onClick={() => setCount(count + 2)}>Click2</button>
+        <button onClick={() => setCount(count + 3)}>Click3</button>
     </div>
   )
 }
